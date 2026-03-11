12ab347a500a1c58ebcba408bee84e03ecd626a9
Alex Kumpula <alex.kumpula01@gmail.com>
2026-03-11 03:04:32 -0600

    test

--- src/niri-dms.nix
+++ src/niri-dms.nix
@@ -9,7 +9,7 @@
       "Mod+D".spawn-sh = let
         dmsPkg = inputs.dms.packages.${pkgs.system}.dms-shell;
       in 
-        "GDK_BACKEND=wayland ${dmsPkg}/bin/dms run";
+        "${dmsPkg}/bin/dms run";
     };
     config.settings.spawn-at-startup = [
       "${inputs.self.packages.${pkgs.system}.alacritty-example}/bin/alacritty"
