d1b763908bec50216f8adde616c34b0618e50ac9
Alex Kumpula <alex.kumpula01@gmail.com>
2026-03-11 00:44:36 -0600

    test

--- src/temp.nix
+++ src/temp.nix
@@ -1,4 +1,12 @@
 { inputs, ... }:
 {
-  
+  flake.wrappers.xalaynix = 
+  { lib, ... }: 
+  {
+    options.xalaynix.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable Xalaynix configuration.";
+    };
+  };
 }
