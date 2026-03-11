14ecdb1435d01c892795a0dc94eea9950d6307c0
Alex Kumpula <alex.kumpula01@gmail.com>
2026-03-11 01:59:38 -0600

    test

--- src/niri-dms.nix
+++ src/niri-dms.nix
@@ -5,11 +5,14 @@
     imports = [ wlib.wrapperModules.niri ];
     settings.binds = {
       "Mod+T".spawn-sh = "${inputs.self.packages.${pkgs.system}.alacritty-example}/bin/alacritty";
-      "Mod+D".spawn-sh = "${inputs.dms.packages.${pkgs.system}.dms-shell}/bin/dms run --session > /tmp/dms-debug.log 2>&1";
+      "Mod+D".spawn-sh = "${inputs.dms.packages.${pkgs.system}.dms-shell}/bin/dms run --session";
     };
     settings.spawn-at-startup = [
       "${inputs.self.packages.${pkgs.system}.alacritty-example}/bin/alacritty"
       "${inputs.dms.packages.${pkgs.system}.dms-shell}/bin/dms"
     ];
+    settings.env = {
+      "PATH" = "${inputs.dms.packages.${pkgs.system}.dms-shell}/bin" + (if pkgs.stdenv.isLinux then ":" else ":") + "$PATH";
+    };
   };
 }
