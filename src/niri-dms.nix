7357ab137acd082e5ea5cb599cd234d4d490a5ef
Alex Kumpula <alex.kumpula01@gmail.com>
2026-03-11 03:12:01 -0600

    test

--- src/niri-dms.nix
+++ src/niri-dms.nix
@@ -4,17 +4,21 @@
   { pkgs, wlib, ... }: 
   let
     dmsPkg = inputs.dms.packages.${pkgs.system}.dms-shell;
+    alacrittyPkg = inputs.self.packages.${pkgs.system}.alacritty-example;
   in
   {
     imports = [ wlib.wrapperModules.niri ];
     config.settings.binds = {
-      "Mod+T".spawn-sh = "${inputs.self.packages.${pkgs.system}.alacritty-example}/bin/alacritty -e sh -c 'echo ${inputs.dms.packages.${pkgs.system}.default}; read'";
-      # "Mod+D".spawn-sh = "${inputs.dms.packages.${pkgs.system}.dms-shell}/bin/dms run --session";
+      "Mod+T".spawn-sh = "${alacrittyPkg}/bin/alacritty";
       "Mod+D".spawn-sh = "${dmsPkg}/bin/dms run --session";
     };
     config.settings.spawn-at-startup = [
-      "${inputs.self.packages.${pkgs.system}.alacritty-example}/bin/alacritty"
-      "${inputs.dms.packages.${pkgs.system}.dms-shell}/bin/dms"
+      "${alacrittyPkg}/bin/alacritty"
+      [
+        "${dmsPkg}/bin/dms"
+        "run"
+        "--session"
+      ]
     ];
     config.env = {
       GDK_BACKEND = "wayland";
