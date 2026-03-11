d99e51963183a2ebf300aa68bd751275b97bcad9
Alex Kumpula <alex.kumpula01@gmail.com>
2026-03-11 01:39:51 -0600

    test

--- src/niri-dms.nix
+++ src/niri-dms.nix
@@ -3,10 +3,11 @@
   flake.wrappers.niri-dms = 
   { pkgs, wlib, ... }: {
     imports = [ wlib.wrapperModules.niri ];
-    # settings.terminal.shell.program = "${pkgs.zsh}/bin/zsh";
-    # settings.terminal.shell.args = [ "-l" ];
     settings.binds = {
       "Mod+T".spawn-sh = "${inputs.self.packages.${pkgs.system}.alacritty-example}/bin/alacritty";
     };
+    settings.spawn-at-startup = [
+      "${inputs.self.packages.${pkgs.system}.alacritty-example}/bin/alacritty"
+    ];
   };
 }
