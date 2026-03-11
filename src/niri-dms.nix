f9517acadf59eccd6be2a78d8e81ce9702f4978f
Alex Kumpula <alex.kumpula01@gmail.com>
2026-03-11 01:50:48 -0600

    test

--- src/niri-dms.nix
+++ src/niri-dms.nix
@@ -5,6 +5,7 @@
     imports = [ wlib.wrapperModules.niri ];
     settings.binds = {
       "Mod+T".spawn-sh = "${inputs.self.packages.${pkgs.system}.alacritty-example}/bin/alacritty";
+      "Mod+D".spawn-sh = "${inputs.dms.packages.${pkgs.system}.dms-shell}/bin/dms";
     };
     settings.spawn-at-startup = [
       "${inputs.self.packages.${pkgs.system}.alacritty-example}/bin/alacritty"
