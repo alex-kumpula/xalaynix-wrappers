5cf61191a6a9c912e213603f7446f7c50fa8ec77
Alex Kumpula <alex.kumpula01@gmail.com>
2026-03-11 00:52:24 -0600

    test

--- flake/wrappers.nix
+++ flake/wrappers.nix
@@ -1,12 +1,14 @@
-{ lib, flake-parts-lib, ... }:
+{ lib, flake-parts-lib, inputs, ... }:
 {
   # Required to define the `wrappers` flake output.
-  options = {
-    flake = flake-parts-lib.mkSubmoduleOptions {
-      wrappers = lib.mkOption {
-        type = with lib.types; lazyAttrsOf raw;
-        default = { };
-      };
-    };
-  };
+  # options = {
+  #   flake = flake-parts-lib.mkSubmoduleOptions {
+  #     wrappers = lib.mkOption {
+  #       type = with lib.types; lazyAttrsOf raw;
+  #       default = { };
+  #     };
+  #   };
+  # };
+
+  imports = [ inputs.wrappers.flakeModules.wrappers ];
 }
