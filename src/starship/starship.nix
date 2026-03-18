1beda9443fda7e5cc3bf9bf606d5664eff3a1377
Alex Kumpula <alex.kumpula01@gmail.com>
2026-03-17 22:38:44 -0600

    test

--- src/starship/starship.nix
+++ src/starship/starship.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ inputs,... }:
 {
   flake.wrappers.starship = 
   { pkgs, wlib, ... }: {
@@ -9,8 +9,8 @@
     config.package = pkgs.starship;
 
     # Point to your starship.toml file in the Nix store
-    config.env = {
-      STARSHIP_CONFIG = "${./starship.toml}";
-    };
+    # config.env = {
+    #   STARSHIP_CONFIG = "${./starship.toml}";
+    # };
   };
 }
