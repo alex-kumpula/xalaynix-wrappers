f6af978e0ab345ccb6be05c2b9b15420cca8dbc4
Alex Kumpula <alex.kumpula01@gmail.com>
2026-03-17 23:28:59 -0600

    test

--- src/zsh/config/completion.zsh
+++ src/zsh/config/completion.zsh
@@ -11,17 +11,9 @@ zstyle ':completion:*' menu select
 
 
 # --- Auto-suggestions ---
-
-# Look for the plugin in the Nix profile paths
-# This finds where Nix placed the 'zsh-autosuggestions' package
-if [[ -f "/run/current-system/sw/share/zsh-autosuggestions/zsh-autosuggestions.zsh" ]]; then
-    source "/run/current-system/sw/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
-elif [[ -f "$HOME/.nix-profile/share/zsh-autosuggestions/zsh-autosuggestions.zsh" ]]; then
-    source "$HOME/.nix-profile/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
+if [[ -n "$AUTOSUGGEST_SRC" ]]; then
+    source "$AUTOSUGGEST_SRC"
 fi
 
-# Customize the suggestion color (e.g., dim gray)
-ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=8"
-
-# Bind 'Right Arrow' or 'End' to accept the suggestion
+# Use Right-Arrow to accept the ghost text
 bindkey '^[[C' forward-char
