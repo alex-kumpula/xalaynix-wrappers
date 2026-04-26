4846f3e403b12186b8b5be1f1678615a0903dda3
Alex Kumpula <alex.kumpula01@gmail.com>
2026-04-25 18:34:21 -0600

    test

--- src/zed-editor/zed-editor.nix
+++ src/zed-editor/zed-editor.nix
@@ -1,13 +1,13 @@
-{ pkgs, ... }:
+{ ... }:
 {
-  flake.wrappers.zed-editor =
+  flake.wrappers.zed =
     { pkgs, wlib, ... }: {
       imports = [ wlib.modules.default ];
       config.package = pkgs.zed-editor;
-      config.run = command_string: ''
+      config.argv0type = command_string: ''
         exec ${pkgs.bubblewrap}/bin/bwrap \
           --dev-bind / / \
-          --ro-bind ${./settings.json} "$XDG_DATA_HOME/zed/settings.json" \
+          --ro-bind ${./settings.json} "''${XDG_DATA_HOME:-$HOME/.local/share}/zed/settings.json" \
           -- ${command_string} "$@"
       '';
     };
