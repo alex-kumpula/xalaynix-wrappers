b9c06698302173e1b0c5354265aa365f9eef9b73
Alex Kumpula <alex.kumpula01@gmail.com>
2026-05-11 16:26:10 -0600

    Test

--- src/zed-editor/zed-editor.nix
+++ src/zed-editor/zed-editor.nix
@@ -20,10 +20,15 @@
       imports = [ wlib.modules.default ];
       config.package = pkgs.writeShellScriptBin "zed-editor" ''
         #!${pkgs.bash}/bin/bash
-          ${pkgs.bubblewrap}/bin/bwrap \
-            --dev-bind / / \
-            --ro-bind ${./settings.json} "''${XDG_CONFIG_HOME:-$HOME/.config}/zed/settings.json" \
-            -- ${pkgs.zed-editor}/bin/zeditor "$@"
+        
+        # Ensure the config directory exists on the host so bwrap can use it as a mount point
+        CONF_DIR="''${XDG_CONFIG_HOME:-$HOME/.config}/zed"
+        mkdir -p "$CONF_DIR"
+
+        exec ${pkgs.bubblewrap}/bin/bwrap \
+          --dev-bind / / \
+          --ro-bind ${./settings.json} "$CONF_DIR/settings.json" \
+          -- ${pkgs.zed-editor}/bin/zeditor "$@"
       '';
     };
 }
