99f2daf8746b04d5f0f9379a75a811e273ac2641
Alex Kumpula <alex.kumpula01@gmail.com>
2026-05-11 00:56:02 -0600

    Fix

--- src/zed-editor/zed-editor.nix
+++ src/zed-editor/zed-editor.nix
@@ -1,35 +1,14 @@
 { ... }:
 {
   flake.wrappers.zed =
-    { pkgs, wlib, lib, ... }: {
+    { pkgs, wlib, ... }: {
       imports = [ wlib.modules.default ];
-      config.package = pkgs.writeShellScriptBin "zed-wrapped" ''
-          exec ${pkgs.bubblewrap}/bin/bwrap \
-            --dev-bind / / \
-            --ro-bind ${./settings.json} \
-              "''${XDG_CONFIG_HOME:-$HOME/.config}/zed/settings.json" \
-            -- ${lib.getExe pkgs.zed-editor} "$@"
-        '';
+      config.package = pkgs.zed-editor;
+      config.argv0type = command_string: ''
+        exec ${pkgs.bubblewrap}/bin/bwrap \
+          --dev-bind / / \
+          --ro-bind ${./settings.json} "''${XDG_CONFIG_HOME:-$HOME/.config}/zed/settings.json" \
+          -- ${command_string} "$@"
+      '';
     };
 }
-
-
-# { ... }:
-# {
-#   flake.wrappers.zed =
-#   { pkgs, wlib, ... }: {
-#     flake.wrappers.zed = wlib.wrapModule ({ config, lib, wlib, ... }: {
-#       imports = [ wlib.modules.default ];
-#       config = {
-#         package = pkgs.zed-editor;
-#         command = pkgs.writeShellScript "zed-wrapped" ''
-#           exec ${pkgs.bubblewrap}/bin/bwrap \
-#             --dev-bind / / \
-#             --ro-bind ${./settings.json} \
-#               "''${XDG_CONFIG_HOME:-$HOME/.config}/zed/settings.json" \
-#             -- ${lib.getExe config.package} "$@"
-#         '';
-#       };
-#     });
-#   };
-# }
