69a4f06a82655210c07a164e0d0fadceb0a80fe2
Alex Kumpula <alex.kumpula01@gmail.com>
2026-05-11 16:22:59 -0600

    test

--- src/noctalia-wrapped.nix
+++ src/noctalia-wrapped.nix
@@ -8,9 +8,14 @@
   { pkgs, wlib, ... }: {
     imports = [ wlib.modules.default ];
     config.package = noctaliaPkg;
-    config.flags."--config-file" = "${./.wezterm.lua}";
     config.env = {
       TESTVAR = "Hello from wrapped Noctalia! :D";
     };
+    config.argv0type = command_string: ''
+        exec ${pkgs.bubblewrap}/bin/bwrap \
+          --dev-bind / / \
+          --bind ${./settings.json} "''${XDG_CONFIG_HOME:-$HOME/.config}/niri/noctalia.kdl" \
+          -- ${command_string} "$@"
+      '';
   };
 }
--- src/zed-editor/zed-editor.nix
+++ src/zed-editor/zed-editor.nix
@@ -1,14 +1,29 @@
+# { ... }:
+# {
+#   flake.wrappers.zed =
+#     { pkgs, wlib, ... }: {
+#       imports = [ wlib.modules.default ];
+#       config.package = pkgs.zed-editor;
+#       config.argv0type = command_string: ''
+#         exec ${pkgs.bubblewrap}/bin/bwrap \
+#           --dev-bind / / \
+#           --ro-bind ${./settings.json} "''${XDG_CONFIG_HOME:-$HOME/.config}/zed/settings.json" \
+#           -- ${command_string} "$@"
+#       '';
+#     };
+# }
+
 { ... }:
 {
   flake.wrappers.zed =
     { pkgs, wlib, ... }: {
       imports = [ wlib.modules.default ];
-      config.package = pkgs.zed-editor;
-      config.argv0type = command_string: ''
-        exec ${pkgs.bubblewrap}/bin/bwrap \
-          --dev-bind / / \
-          --ro-bind ${./settings.json} "''${XDG_CONFIG_HOME:-$HOME/.config}/zed/settings.json" \
-          -- ${command_string} "$@"
+      config.package = pkgs.writeShellScriptBin "zed-editor" ''
+        #!${pkgs.bash}/bin/bash
+          exec ${pkgs.bubblewrap}/bin/bwrap \
+            --dev-bind / / \
+            --ro-bind ${./settings.json} "''${XDG_CONFIG_HOME:-$HOME/.config}/zed/settings.json" \
+            -- ${pkgs.zed-editor}/bin/zed "$@"
       '';
     };
 }
