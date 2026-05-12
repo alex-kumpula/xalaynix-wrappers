6572b211cc022c571f4dde6e6da87ab1e35b0dbe
Alex Kumpula <alex.kumpula01@gmail.com>
2026-05-11 23:01:38 -0600

    test

--- src/niri-noctalia.nix
+++ src/niri-noctalia.nix
@@ -16,6 +16,7 @@
         TARGET_FILE="$ZED_CONFIG_DIR/settings.json"
         CUSTOM_SETTINGS="$(realpath ${./niri-noctalia.nix})"
         export TESTVAR2="HI :D"
+        export PATH="${niriPkg}/bin:$PATH"
         exec ${pkgs.bubblewrap}/bin/bwrap \
           --dev-bind / / \
           --bind "$CUSTOM_SETTINGS" "$TARGET_FILE" \
