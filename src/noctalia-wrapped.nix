a140c2f8a107e3945b4c64117ba7543534051315
Alex Kumpula <alex.kumpula01@gmail.com>
2026-05-11 21:09:59 -0600

    test

--- src/noctalia-wrapped.nix
+++ src/noctalia-wrapped.nix
@@ -1,22 +1,23 @@
 { inputs, ... }:
 {
   flake.wrappers.noctalia-wrapped = 
-  { pkgs, wlib, ... }: 
+  { pkgs, wlib, lib, ... }: 
   let
     noctaliaPkg = inputs.noctalia.packages.${pkgs.system}.default;
   in
   {
-    imports = [ wlib.modules.default ];
-    config.package = pkgs.writeShellScriptBin "noctalia-shell" ''
-        #!${pkgs.bash}/bin/bash
-        ZED_CONFIG_DIR="$HOME/.config/zed"
-        TARGET_FILE="$ZED_CONFIG_DIR/settings.json"
-        CUSTOM_SETTINGS="$(realpath ${./niri-noctalia.nix})"
-        exec ${pkgs.bubblewrap}/bin/bwrap \
-          --dev-bind / / \
-          --bind "$CUSTOM_SETTINGS" "$TARGET_FILE" \
-          -- ${noctaliaPkg}/bin/noctalia-shell "$@"
-      '';
+    imports = [ wlib.modules.noctalia-shell ];
+    # config.package = pkgs.writeShellScriptBin "noctalia-shell" ''
+    #     #!${pkgs.bash}/bin/bash
+    #     ZED_CONFIG_DIR="$HOME/.config/zed"
+    #     TARGET_FILE="$ZED_CONFIG_DIR/settings.json"
+    #     CUSTOM_SETTINGS="$(realpath ${./niri-noctalia.nix})"
+    #     exec ${pkgs.bubblewrap}/bin/bwrap \
+    #       --dev-bind / / \
+    #       --bind "$CUSTOM_SETTINGS" "$TARGET_FILE" \
+    #       -- ${noctaliaPkg}/bin/noctalia-shell "$@"
+    #   '';
+    config.package = lib.mkForce noctaliaPkg;
     config.env = {
       TESTVAR = "Hello from wrapped Noctalia! :D";
     };
