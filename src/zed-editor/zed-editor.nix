98b247863299070538c052c9315851e2232bdeb5
Alex Kumpula <alex.kumpula01@gmail.com>
2026-05-11 18:13:33 -0600

    Test

--- src/zed-editor/zed-editor.nix
+++ src/zed-editor/zed-editor.nix
@@ -1,36 +1,34 @@
-{ ... }:
-{
-  flake.wrappers.zed =
-    { pkgs, wlib, ... }: {
-      imports = [ wlib.modules.default ];
-      config.package = pkgs.zed-editor;
-      config.argv0type = command_string: ''
-        exec ${pkgs.bubblewrap}/bin/bwrap \
-          --dev-bind / / \
-          --ro-bind ${./settings.json} "''${XDG_CONFIG_HOME:-$HOME/.config}/zed/settings.json" \
-          -- ${command_string} "$@"
-      '';
-    };
-}
-
 # { ... }:
 # {
 #   flake.wrappers.zed =
 #     { pkgs, wlib, ... }: {
 #       imports = [ wlib.modules.default ];
-      
-#       # We define the package as a shell script that calls bwrap directly
-#       config.package = pkgs.writeShellScriptBin "zed" ''
-#         #!${pkgs.bash}/bin/bash
-        
-#         # Ensure the destination path exists (bwrap needs a mount point)
-#         TARGET="''${XDG_CONFIG_HOME:-$HOME/.config}/zed/settings.json"
-#         mkdir -p "$(dirname "$TARGET")"
-
+#       config.package = pkgs.zed-editor;
+#       config.argv0type = command_string: ''
 #         exec ${pkgs.bubblewrap}/bin/bwrap \
 #           --dev-bind / / \
-#           --ro-bind ${./settings.json} "$TARGET" \
-#           -- ${pkgs.zed-editor}/bin/zeditor "$@"
+#           --ro-bind ${./settings.json} "''${XDG_CONFIG_HOME:-$HOME/.config}/zed/settings.json" \
+#           -- ${command_string} "$@"
 #       '';
 #     };
 # }
+
+{ ... }:
+{
+  flake.wrappers.zed =
+    { pkgs, wlib, ... }: {
+      imports = [ wlib.modules.default ];
+      
+      # We define the package as a shell script that calls bwrap directly
+      config.package = pkgs.writeShellScriptBin "zed" ''
+        #!${pkgs.bash}/bin/bash
+        ZED_CONFIG_DIR="$HOME/.config/zed"
+        TARGET_FILE="$ZED_CONFIG_DIR/settings.json"
+        CUSTOM_SETTINGS="$(realpath ${./settings.json})"
+        exec bwrap \
+          --dev-bind / / \
+          --bind "$CUSTOM_SETTINGS" "$TARGET_FILE" \
+          -- ${pkgs.zed-editor}/bin/zeditor "$@"
+      '';
+    };
+}
