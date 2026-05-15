59e2ea8d5712253eb439228c0af6d06a01ca8438
Alex Kumpula <alex.kumpula01@gmail.com>
2026-05-14 23:53:23 -0600

    Update

--- src/niri-noctalia/niri-noctalia.nix
+++ src/niri-noctalia/niri-noctalia.nix
@@ -13,6 +13,20 @@
     config = {
       package = lib.mkForce niriPkg;
 
+      argv0type = command_string: ''
+        exec -a "$0" ${pkgs.bubblewrap}/bin/bwrap \
+          --bind / / \
+          --dev /dev \
+          --proc /proc \
+          ${command_string}
+      '';
+
+      runShell = [
+        ''
+          mkdir -p $HOME/testdir
+        ''
+      ];
+
       "config.kdl".content = /* kdl */ ''
         binds {
           Mod+Shift+Slash { show-hotkey-overlay; }
